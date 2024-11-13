95dd4376e5ba61a130e98e3f94cf45c042dd93bf
David69699 <abutidzedato18@gmail.com>
2024-11-13 17:33:02 +0400

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,124 @@
+"use client";
 import Image from "next/image";
+import Location from "../../public/images/location.png";
+import USA from "../../public/images/usa.svg";
+import GEO from "../../public/images/geo.svg";
+import building from "../../public/images/building-2.svg";
+import mail from "../../public/images/mail.svg";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+    <div className="flex flex-col md:flex-row space-y-8 md:space-y-0 md:space-x-8 p-8">
+      <div className="bg-green-50 p-[30px] rounded-[20px] shadow-lg w-full md:max-w-[470px] min-h-[652px] flex-grow h-[600px]">
+        <h2 className="text-black text-2xl font-medium leading-normal mb-4">
+          Get In Touch!
+        </h2>
+        <p className="text-gray-600 mb-[30px]">Send us your message anytime</p>
+        <form className="space-y-4 flex flex-col gap-4">
+          <div className="flex space-x-4">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="w-1/2 p-2 border border-black bg-transparent rounded-[10px]"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="w-1/2 p-2 border border-black bg-transparent rounded-[10px]"
+            />
+          </div>
+          <div className="relative">
+            <input
+              className="flex items-center pl-10 border border-black bg-transparent p-2 rounded-[10px] w-full"
+              placeholder="Your Email"
+            />
+            <div className="absolute left-3 top-2.5">
+              <Image src={mail} alt="Mail Icon" width={20} height={20} />
+            </div>
+          </div>
+
+          <div className="relative">
+            <input
+              className="flex items-center pl-10 border border-black bg-transparent p-2 rounded-[10px] w-full"
+              placeholder="Your Company"
+            />
+            <div className="absolute left-3 top-2.5">
+              <Image
+                src={building}
+                alt="Building Icon"
+                width={20}
+                height={20}
+              />
+            </div>
+          </div>
+          <textarea
+            placeholder="Send Us A Message"
+            className="w-full p-2 border border-black rounded-[10px] h-24 bg-transparent"
+          ></textarea>
+          <button
+            type="submit"
+            className="w-full bg-green-600 text-white p-2 rounded-[50px] hover:bg-green-700"
           >
-            Read our docs
+            Submit
+          </button>
+        </form>
+        <p className="text-black text-center font-poppins text-base font-normal leading-normal mt-5">
+          By sending us the message, you agree to our
+          <br />
+          <a href="#" className="text-black font-semibold">
+            terms and conditions
           </a>
+        </p>
+      </div>
+
+      <div className="bg-green-50 pt-5 pb-[28px] pr-[28px] pl-5 rounded-lg shadow-lg w-full md:w-3/5 h-[534px] self-end flex flex-col md:flex-row items-center md:items-start">
+        <div className="space-y-4 w-full md:w-1/2">
+          <h2 className="text-xl font-bold mb-2 text-gray-800">Locations</h2>
+          <p className="text-gray-600 mb-6">
+            Contact with Us at These Locations
+          </p>
+          <div className="space-y-6 flex flex-col justify-between w-full">
+            {" "}
+            <div className="flex items-center space-x-3 border-b-[3px] border-customGreen rounded-[10px] p-[10px]">
+              {" "}
+              <Image src={USA} alt="" width={25} height={15} />
+              <p className="text-gray-800 pb-1 text-sm">
+                {" "}
+                USA, San Francisco, Valencia St. 918
+              </p>
+            </div>
+            <div className="flex items-center space-x-3 border-b-[3px] border-customGreen rounded-[10px] p-[10px]">
+              <Image src={USA} alt="" width={25} height={15} />
+              <p className="text-gray-800 pb-1 text-sm">
+                USA, San Francisco, Valencia St. 918
+              </p>
+            </div>
+            <div className="flex items-center space-x-3 border-b-[3px] border-customGreen rounded-[10px] p-[10px]">
+              <Image src={GEO} alt="" width={25} height={15} />
+              <p className="text-gray-800 pb-1 text-sm">
+                Georgia, Chavchavadze St. 12
+              </p>
+            </div>
+            <div className="flex items-center space-x-3 border-b-[3px] border-customGreen rounded-[10px] p-[10px]">
+              <Image src={GEO} alt="" width={25} height={15} />
+              <p className="text-gray-800 pb-1 text-sm">
+                Georgia, Chavchavadze St. 12
+              </p>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+
+        {/* Location Map Image */}
+        <div className="mt-8 md:mt-0 md:ml-8 w-[365px] h-[300px]">
           <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
+            src={Location}
+            alt="Location Map"
+            width={365}
+            height={300}
+            className="w-[365px] h-[486px]"
           />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+      </div>
     </div>
   );
 }
